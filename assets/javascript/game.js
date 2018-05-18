0f8941c9ea1dc3df8a79258d4891ec47084cb012
dotRollen <edwardnnz@gmail.com>
2018-05-18 14:29:39 -0400

    added if statement to check if game is running, if not and player presses enter a new game will begin. span element is created for every letter of the word and appended to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,54 +9,79 @@ var wargame = {
     ],
     pressStart: false,
     wordPicked: "",
-    wordSplit: [],
-    pickWord: function(){
+    wordSplitted: [],
+    pickWord: function () {
         this.wordPicked = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
-        console.log("I chose this word " + this.wordPicked)
         return this.wordPicked;
     },
-    splitWord: function(word){
-        this.wordSplit = word.split("")
-        console.log("Word is now split as " + this.wordSplit)
+    splitWord: function (word) {
+        this.wordSplit = word.split("");
+        return this.wordSplit;
     },
-    ifExist: function(key) {
+    ifExist: function (key) {
         for (var i = 0; i < this.lettersTried.length; i++) {
             if (key == (this.lettersTried[i])) {
                 return true; 
             }
         }
     },
+    setAttr: function (elem, attrs) {
+        for (var key in attrs) {
+            elem.setAttribute(key, attrs[key]);
+            elem.innerHTML = "&nbsp;";
+        }
+        return elem;
+    },
 }
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     
-
-
     if (wargame.pressStart) {
 
         if ((event.keyCode >= 65) && (event.keyCode <= 90)){
             if (triesLeft >= 1) {
+
                 var userGuess = event.key.toLowerCase();
+
+                for (var i = 0; i < wargame.wordSplitted; i++) {
+                    
+                    if (userGuess == wargame.wordSplitted[i]) {
+                        
+                    }
+                }
             }
 
             else {
-                alert("Game over!");
+                alert("Game over man!")
             }
-            
-            var userGuess = event.key.toLowerCase();
-
         }
     }
+
     else {
 
         if (event.keyCode == 13) {
             
             wargame.splitWord(wargame.pickWord());
 
+            document.getElementById("word").innerHTML = "";
+
+            for (var i = 0; i < wargame.wordSplitted.length; i++) {
+
+                var main = document.getElementById("word");
+                var letterSpan = document.createElement("span");
+
+                main.append(
+                    wargame.setAttr(letterSpan, {
+                        "data-letter": wargame.wordSplitted[i],
+                        "class": "word-letter",
+                    })
+                );
+            }
+
             document.getElementById("tries-left").innerHTML = wargame.triesLeft = 12;
             document.getElementById("letters-tried").innerHTML = wargame.lettersTried = [];
 
-            //wargame.pressStart = true;
+            wargame.pressStart = true;
         }
     }
 }
